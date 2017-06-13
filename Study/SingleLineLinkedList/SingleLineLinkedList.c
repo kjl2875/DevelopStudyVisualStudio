aaa68e0355c628f08b5164c15165922519394f55
PUG <kjl2875@gmail.com>
2017-06-13 23:05:13 +0900

    1차원 linkedlist: add, move, del 막짜놓은거라 아직 손댈게 많다.

--- Study/SingleLineLinkedList/SingleLineLinkedList.c
+++ Study/SingleLineLinkedList/SingleLineLinkedList.c
@@ -12,6 +12,10 @@ void doTest(void);
 1억개의 노드생성 및 dataset, 그리고 정상적으로 삭제한다.
 */
 void doTest2(void);
+/*
+UserInterface 적용중
+*/
+void doTest3(void);
 
 int main(int argc, char *args[])
 {
@@ -25,7 +29,8 @@ int main(int argc, char *args[])
 			"What Test?\n"\
 			"1. 1억개의 노드생성 및 개수체크하고 정상적으로 삭제한다.\n"\
 			"2. 1억개의 노드생성 및 dataset, 그리고 정상적으로 삭제한다.\n"\
-			"0. 종료\n"
+			"3. UserInterface 적용중.\n"\
+			"0. 종료\n"\
 			"Number>"
 		);
 		sw = getch();
@@ -39,12 +44,169 @@ int main(int argc, char *args[])
 		case '2':
 			doTest2();
 			break;
+		case '3':
+			doTest3();
+			break;
 		default:
 			break;
 		}
 	} while (sw != '0');
 	
 	return 0;
+}
+
+void doTest3() // TODO 프로그램 부피가 커지고 같은기능 반복할 일이 생겨서 파일로 따로 쪼개야될듯, 게다가 cut을 활용한 동작 다중리스트도 해야됨.
+{
+	int c;
+	const char *helpMessage = "q: exit, h: help, a: add, arrow: move, d: del\n";
+	struct NODE *list = NULL;
+	struct NODE *tail = NULL;
+	struct NODE *cursor = NULL;
+	struct NODE *tNode;
+	int cursorPosition = 0;
+	int nodeCount = 0;
+
+	printf(helpMessage);
+
+	do
+	{
+		c = getch();
+
+#define USE_SWITCH
+#ifdef USE_SWITCH // Q) SWITCH쓰는게 빠를까? if-else if-else if쓰는게 빠를까?
+		switch (c)
+		{
+		case 'h':
+		{ // Q) 왜 유독 switch만 자동정렬을 1-depth낮게 맞춰주는걸까?
+			printf(helpMessage);
+			break;
+		}
+		case 'a':
+		{
+			if (list != NULL)
+			{
+				insertNodeBack(tail, createNode(NULL));
+				tail = tail->backNode;
+			}
+			else
+			{
+				list = createNode(NULL);
+				tail = list;
+				cursor = list;
+			}
+
+			printf("nodeCount: %d\n", ++nodeCount);
+			break;
+		}
+		case 224: // 0xE0 // Q) 화살표 누르면 왜 이값이 튀어나오게 했을까?
+		{
+			switch (getch())
+			{
+			case 72: // up
+				break;
+			case 80: // down
+				break;
+			case 75: // left
+			{
+				if (cursor == NULL)
+				{
+					printf("nodeCount is 0\n");
+					break;
+				}
+
+				if (cursor->frontNode != NULL)
+				{
+					cursor = cursor->frontNode;
+					--cursorPosition;
+				}
+				
+				printf("cursorPosition = %d\n", cursorPosition);
+				break;
+			}
+			case 77: // right
+			{
+				if (cursor == NULL)
+				{
+					printf("nodeCount is 0\n");
+					break;
+				}
+
+				if (cursor->backNode != NULL)
+				{
+					cursor = cursor->backNode;
+					++cursorPosition;
+				}
+				
+				printf("cursorPosition = %d\n", cursorPosition);
+				break;
+			}
+			default:
+			{
+				break;
+			}
+			}
+
+			break;
+		}
+		case 'd':
+		{
+			if (cursor == NULL)
+			{
+				printf("nodeCount is 0\n");
+				break;
+			}
+
+			tNode = cursor;
+
+			if (cursor->frontNode != NULL)
+			{
+				cursor = cursor->frontNode;
+				--cursorPosition;
+
+				if (tNode == tail)
+				{
+					tail = cursor;
+				}
+			}
+			else if(cursor->backNode != NULL)
+			{
+				cursor = cursor->backNode;
+
+				if (tNode == list)
+				{
+					list = cursor;
+				}
+			}
+			else
+			{
+				cursor = NULL;
+				list = NULL;
+				tail = NULL;
+			}
+
+			delNode(tNode);
+			--nodeCount;
+
+			printf("cursorPosition = %d\n", cursorPosition);
+			printf("nodeCount = %d\n", nodeCount); // Q) 성능적으로는 --nodeCount가 여기와야 맞긴할것같은데, 기능단위 가독성때문에 이렇게 했다고 치고 이게만약 쓸모가 많아서 여기저기 복붙되다가 몇억개가 될수도 있는데 괜찮을까?
+			
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+#else
+		// 여긴 코드가 없어요.
+#endif
+
+	} while (c != 'q');
+
+	if (list != NULL)
+	{
+		delNodeList(list);
+	}
 }
 
 void doTest2()
